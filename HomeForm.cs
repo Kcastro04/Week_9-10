3cb2c7b1803aab875cbca4b07d72a41e7582fdb5
Kcastro04 <kharellynjoycecastro@gmail.com>
2026-05-29 22:38:42 +0800

    HomeForm.cs

---
+++ HomeForm.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace CrystalReportsDashboard
+{
+    public partial class HomeForm : Form
+    {
+        public HomeForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
